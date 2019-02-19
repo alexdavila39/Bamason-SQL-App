d69a6990730d19aa46c79f85180206b611caa334
Alejandro <alexdavila39@gmail.com>
2019-02-19 18:33:29 -0500

    Fully Working Code

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -25,9 +25,9 @@ var queryProducts = function () {
         console.log("_____________________________________________________________________")
         console.log(chalk.inverse("Item ID || Product Name || Department || Price || Invetory ||"))
         console.log("---------------------------------------------------------------------")
-        if(err) throw err;
+        if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(chalk.blue(res[i].item_id) + " || " +(chalk.yellow(res[i].product_name)) + " || " + (chalk.cyan(res[i].department_name)) + " || " +(chalk.green(res[i].price)) + " || " +(chalk.red(res[i].stock_quantity)) + "\n");
+            console.log(chalk.blue(res[i].item_id) + " || " + (chalk.yellow(res[i].product_name)) + " || " + (chalk.cyan(res[i].department_name)) + " || " + (chalk.green(res[i].price)) + " || " + (chalk.red(res[i].stock_quantity)) + "\n");
         };
         console.log("------------------------------------------------------");
 
@@ -45,14 +45,14 @@ var promptCustomer = function (res) {
             }
         ]).then(function (answer) {
             var correct = false;
-//If user is not ready to make a purchase or does not want to make a second puchase they can Exit the App by pressing "Q".
+            //If user is not ready to make a purchase or does not want to make a second puchase they can Exit the App by pressing "Q".
             if (answer.choice.toUpperCase() == "Q")
                 process.exit();
             for (var i = 0; i < res.length; i++) {
                 if (res[i].item_id == answer.choice) {
                     correct = true;
                     var product = answer.choice;
-                    var id= i;
+                    var id = i;
                     inquirer.prompt({
                         type: "input",
                         name: "quant",
@@ -65,19 +65,19 @@ var promptCustomer = function (res) {
                             }
                         }
                     }).then(function (answer) {
-                        
-                        if((res[id].stock_quantity - answer.quant)> 0) {
-                            //This will Update the products database after user buys x amount of items.
-                            connection.query("UPDATE products SET stock_quantity = " + (res[id].stock_quantity - answer.quant) + 
-                            " WHERE item_id= " + product, function (err, res2) {
-                                console.log(chalk.inverse.bold("Product Purchased Successfully!"));
-                                var totalCost = (parseFloat(res[0].price) *product.quant).toFixed(2);
-                                console.log("======================================================");
-                                console.log("Total: "+ " $"+ {totalCost})                                
-                                queryProducts();
 
-                       })
-                    }
+                        if ((res[id].stock_quantity - answer.quant) > 0) {
+                            //This will Update the products database after user buys x amount of items.
+                            connection.query("UPDATE products SET stock_quantity = " + (res[id].stock_quantity - answer.quant) +
+                                " WHERE item_id= " + product, function (err, res2) {
+                                    console.log(chalk.inverse.bold("Product Purchased Successfully!"));
+                                    var totalCost = (parseFloat(res[0].price) * answer.quant).toFixed(2);
+                                    console.log("======================================================");
+                                    console.log("Total: " + " $" + totalCost)
+                                    queryProducts();
+
+                                })
+                        }
                         else {
                             //this message will appear only if user select a higher quantity than is available in stock.
                             console.log(chalk.bgRed.bold("Sorry, We do not have the inventory for the amount Requested."));
