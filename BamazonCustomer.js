75d2c6842f1629928a13073b6fe38892b6a008bb
Alejandro <alexdavila39@gmail.com>
2019-02-20 23:23:15 -0500

    ReadME updated

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Josafat18",
+    password: "",
     database: "bamazon_db"
 });
 //setting connection MYsql data base
