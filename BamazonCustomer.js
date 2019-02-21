3f6cfe7968b9b3925d48330ef294055c611298ee
Alejandro <alexdavila39@gmail.com>
2019-02-20 22:50:43 -0500

    Updated code & README

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var chalk = require("chalk")
+var chalk = require("chalk");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -23,13 +24,17 @@ var queryProducts = function () {
     connection.query("SELECT * FROM products ", function (err, res) {
         console.log(chalk.yellow.inverse.italic.bold("Welcome To Bamazon! Please select from the list of products below."))
         console.log("_____________________________________________________________________")
-        console.log(chalk.inverse("Item ID || Product Name || Department || Price || Invetory ||"))
+        
         console.log("---------------------------------------------------------------------")
         if (err) throw err;
+        var table = new Table ({
+            head: [ chalk.blue("ID"), chalk.yellow("PRODUCT"),chalk.cyan("DEPARTMENT"),chalk.green("PRICE"), "QUANITY IN STOCK"]
+        })
         for (var i = 0; i < res.length; i++) {
-            console.log(chalk.blue(res[i].item_id) + " || " + (chalk.yellow(res[i].product_name)) + " || " + (chalk.cyan(res[i].department_name)) + " || " + (chalk.green(res[i].price)) + " || " + (chalk.red(res[i].stock_quantity)) + "\n");
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$"+ res[i].price, res[i].stock_quantity + "\n"]);
         };
-        console.log("------------------------------------------------------");
+        console.log(table.toString() + "\n\n");
+       
 
         promptCustomer(res);
     })
@@ -41,10 +46,13 @@ var promptCustomer = function (res) {
             {
                 type: "input",
                 name: "choice",
-                message: "Enter the ID of the Item you would like to buy. [if not ready, Quit with Q]"
+                message: (chalk.yellow("Enter the ID of the Item you would like to buy.")) + chalk.bgRed.italic("[If not ready, Quit with Q]")
             }
+            
         ]).then(function (answer) {
             var correct = false;
+        console.log("\n");
+
             //If user is not ready to make a purchase or does not want to make a second puchase they can Exit the App by pressing "Q".
             if (answer.choice.toUpperCase() == "Q")
                 process.exit();
@@ -56,7 +64,7 @@ var promptCustomer = function (res) {
                     inquirer.prompt({
                         type: "input",
                         name: "quant",
-                        message: "How many Items would you like to buy?",
+                        message: (chalk.yellow("How many Items would you like to buy?")),
                         validate: function (value) {
                             if (isNaN(value) == false) {
                                 return true;
@@ -65,23 +73,24 @@ var promptCustomer = function (res) {
                             }
                         }
                     }).then(function (answer) {
-
+                        console.log("\n");
                         if ((res[id].stock_quantity - answer.quant) > 0) {
                             //This will Update the products database after user buys x amount of items.
                             connection.query("UPDATE products SET stock_quantity = " + (res[id].stock_quantity - answer.quant) +
                                 " WHERE item_id= " + product, function (err, res2) {
-                                    console.log(chalk.inverse.bold("Product Purchased Successfully!"));
+                                    console.log(chalk.inverse.cyan.bold('"'+res[id].product_name + '"'+" Purchased Successfully!\n"));
                                     var totalCost = (parseInt(res[id].price) * answer.quant).toFixed(2);
-                                    console.log("======================================================");
-                                    console.log(chalk.green("Total: " + " $" + totalCost))
-                                    promptCustomer();
+                                    console.log(chalk.yellow("===========================================================================" + "\n"));
+                                    console.log(chalk.inverse.green("Thank You for shopping with Us!\n"))
+                                    console.log(chalk.green("Your Total is:" + " $" + totalCost+ "\n\n"))
+                                    console.log(chalk.yellow("==========================================================================="))
+                                    promptCustomer(res);
 
                                 })
                         }
                         else {
                             //this message will appear only if user select a higher quantity than is available in stock.
-                            console.log(chalk.bgRed.bold("Sorry, We do not have the inventory for the amount Requested."));
-                            console.log(chalk.bgGreen.bold("*****Please place order for less quantities*****"));
+                            console.log(chalk.inverse.red.bold("\n"+"*****Sorry, we don't have enough in stock to fulfill your order*****\n\n"));
                             promptCustomer(res);
                         }
                     })
@@ -89,7 +98,8 @@ var promptCustomer = function (res) {
             }
             //user must select from items ID's provided if not erros message will appear.
             if (i == res.length && correct == false) {
-                console.log("Not a Valid Selection!");
+                console.log(chalk.inverse.red.bold("\n"+"****Not a Valid Selection!****\n\n"));
+                console.log(chalk.inverse.yellow.italic.bold("***Please choose from the item ID's Provided, Thank you!***\n\n"));
                 promptCustomer(res);
             }
         })
--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Bamazon-SQL-App
 
-###Overview
-* I have created an Amazon-like storefront with the MySQL. The app will take in orders from customers and deplete stock from the store's inventory.
+###  Motivation & Overview 
+*  This is a project for UCF's Coding bootcamp class where we are learning to use Databases thru MySQL Workbench and implementing them on NODE.JS .I have created an Amazon-like storefront with the MySQL. The app will take in orders from customers and deplete stock from the store's inventory.
+
+# Customer Interface
+Initializing node bamazon.js in the command line brings up the storefront. 
+The customer interface allows the user to view the current inventory of store items: item IDs, Product, Department in which the item is located,price and Stock quantity. The user is then able to purchase one of the existing items by entering the item ID and the desired quantity. If the selected quantity is currently in stock, the user's order is fulfilled, displaying the total purchase price and updating the store database. If the desired quantity is not available, the user is prompted to modify their order.
+
+# Customer View
+
+* customer will be be shown a list full of products vailable for them to purchase.
+![start](assets/images/start.png)
 
 
+* He will then be prompted with a commmand: "Enter the ID of the Item you would like to buy.[If not ready, Quit with Q]"
+    * If customer is not ready he can Quit the app by pressing "Q".
+
+* After selecting the product the customer desires he will be asked the following: "How many Items would you like to buy?"
+
+* After customer types a desired amount, he will be shown the name of the product and if it was successfully purchased or an error message if we do not have enough product in stock. 
+
+# Technology Used
+
+* NPM packages (Mysql,Inquirer,Chalk,CLI-table2)
+* NODE.JS
+* MySQL WorkBench
 
